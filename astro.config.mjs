ab5604132859a9ec3057db6f30dd0c10088cfe6c
osho sunkanmi harry <oshosunkanmi270@gmail.com>
2025-02-06 00:13:36 +0100

    made few changes to the dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import sanity from '@sanity/astro';
+import node from '@astrojs/node';
+
+import netlify, { remotePatternToRegex } from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react(), sanity({
+    projectId: 'swaaax0t',
+    dataset: 'production',
+    useCdn: true, //it was set to true from false because it's not a static page
+    studioBasePath: "/admin",
+  })],
+
+  output: 'server', 
+  devToolbar:{
+    enabled: false
+  },
+  adapter: netlify(),
+  
+  image: {
+      remotePatterns: [{protocol: "http"}, {protocol: "https"}]
+    }
+});
